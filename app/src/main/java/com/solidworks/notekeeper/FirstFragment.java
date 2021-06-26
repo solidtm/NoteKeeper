dede00a0aac5360903fe1eaeb70089bf4a5d5af4
solidtm <richardebboh@gmail.com>
2021-06-26 20:54:57 +0100

    Populating Spinner

--- app/src/main/java/com/solidworks/notekeeper/FirstFragment.java
+++ app/src/main/java/com/solidworks/notekeeper/FirstFragment.java
@@ -26,8 +26,8 @@ public class FirstFragment extends Fragment {
         view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
     }
--- app/src/main/java/com/solidworks/notekeeper/NoteActivity.java
+++ app/src/main/java/com/solidworks/notekeeper/NoteActivity.java
@@ -12,24 +12,19 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
 
 public class NoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_note);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Spinner spinnerCourses = findViewById(R.id.spinner_courses);
+
     }
 
     @Override
